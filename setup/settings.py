6bb627a7b8de42307b88f4771cb5c178e5b6a863
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-04 11:48:10 -0300

    Add widget of calender in form

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tickets'
+    'tickets',
+    'tempus_dominus'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPUS_DOMINUS_LOCALIZE=True
