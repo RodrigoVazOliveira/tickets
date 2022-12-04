9901c8f87cdc92dfe69fbe2f7c80fde9578ec773
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-04 15:56:31 -0300

    Add more inputs

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,6 +1,7 @@
 from datetime import date
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from .travel_class import type_class_travel
 
 
 class TicketForm(forms.Form):
@@ -8,3 +9,12 @@ class TicketForm(forms.Form):
     destination = forms.CharField(label='Destino: ', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     date_departure = forms.DateField(label='Data de ida: ', widget=DatePicker())
     date_back = forms.DateField(label='Data de volta:', widget=DatePicker())
+    date_search = forms.DateField(label='Data de pesquisa:', widget=DatePicker(), disabled=True, initial=date.today())
+    travel_class = forms.ChoiceField(label='Tipo de voo', choices=type_class_travel, widget=forms.Select(attrs={'class': 'form-control'}))
+    informations = forms.CharField(
+        label='Informações extras: ',
+        max_length=200,
+        widget=forms.Textarea(attrs={'class' : 'form-control'}),
+        required=False
+    )
+    email = forms.EmailField(label='E-mail: ', widget=forms.EmailInput(attrs={'class' : 'form-control'}))
