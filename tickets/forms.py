b75e6d6c4a5227650a12bd5e298baec7151d4d58
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-04 12:08:08 -0300

    Add style to inputs

--- tickets/forms.py
+++ tickets/forms.py
@@ -4,7 +4,7 @@ from tempus_dominus.widgets import DatePicker
 
 
 class TicketForm(forms.Form):
-    origin = forms.CharField(label='Origem: ', max_length=100)
-    destination = forms.CharField(label='Destino: ', max_length=100)
+    origin = forms.CharField(label='Origem: ', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    destination = forms.CharField(label='Destino: ', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     date_departure = forms.DateField(label='Data de ida: ', widget=DatePicker())
     date_back = forms.DateField(label='Data de volta:', widget=DatePicker())
