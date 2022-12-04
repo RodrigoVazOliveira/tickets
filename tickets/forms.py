c7f75720f2f75bfcdaf227c98e3aa7539c237f3c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-04 16:12:19 -0300

    usage the tweaks" "

--- tickets/forms.py
+++ tickets/forms.py
@@ -5,16 +5,16 @@ from .travel_class import type_class_travel
 
 
 class TicketForm(forms.Form):
-    origin = forms.CharField(label='Origem: ', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    destination = forms.CharField(label='Destino: ', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    origin = forms.CharField(label='Origem: ', max_length=100)
+    destination = forms.CharField(label='Destino: ', max_length=100)
     date_departure = forms.DateField(label='Data de ida: ', widget=DatePicker())
     date_back = forms.DateField(label='Data de volta:', widget=DatePicker())
     date_search = forms.DateField(label='Data de pesquisa:', widget=DatePicker(), disabled=True, initial=date.today())
-    travel_class = forms.ChoiceField(label='Tipo de voo', choices=type_class_travel, widget=forms.Select(attrs={'class': 'form-control'}))
+    travel_class = forms.ChoiceField(label='Tipo de voo', choices=type_class_travel)
     informations = forms.CharField(
         label='Informações extras: ',
         max_length=200,
-        widget=forms.Textarea(attrs={'class' : 'form-control'}),
-        required=False
+        required=False,
+        widget=forms.Textarea()
     )
-    email = forms.EmailField(label='E-mail: ', widget=forms.EmailInput(attrs={'class' : 'form-control'}))
+    email = forms.EmailField(label='E-mail: ')
